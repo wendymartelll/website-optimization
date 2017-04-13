eaec33c7887419725ffed4db431f5c6ad862588b
Anthony Z <anthony.zamarro@gmail.com>
2017-04-12 22:13:18 -0400

    Fourth Commit

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,1 +1,28 @@
-module.exports=function(grunt){grunt.initConfig ({responsive_images:{dev:{options:{engine:'im',sizes:[{name:'extra-small',width:100,suffix:'_0x',quality:85,},{name:'small',width:200,suffix:'_1x',quality:85}]},files:[{expand:true,src:['*.{gif,jpg,png}'],cwd:'img_src/',dest:'img/'}]},},});grunt.loadNpmTasks('grunt-responsive-images');grunt.registerTask('default',['responsive_images'])};
+module.exports=function(grunt) {
+    grunt.initConfig ( {
+        responsive_images: {
+            dev: {
+                options: {
+                    engine:'im', sizes:[ {
+                        name: 'extra-small', width: 80, suffix: '_0x', quality: 90,
+                    }
+                    , {
+                        name: 'small', width: 200, suffix: '_1x', quality: 88
+                    }
+                    ]
+                }
+                , files:[ {
+                    expand:true, src:['*.{gif,jpg,png}'], cwd: 'img_src/', dest: 'img/'
+                }
+                ]
+            }
+            ,
+        }
+        ,
+    }
+    );
+    grunt.loadNpmTasks('grunt-responsive-images');
+    grunt.registerTask('default', ['responsive_images'])
+}
+
+;
